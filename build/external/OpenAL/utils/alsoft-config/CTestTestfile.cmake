c62a5658c10ab8b19ad652083158cd941937423a
HalmonCelman <halmoncelman@gmail.com>
2023-10-18 08:32:33 +0200

    Without build path

--- build/external/OpenAL/utils/alsoft-config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/halmon/Pulpit/KK/OpenAL/external/OpenAL/utils/alsoft-config
-# Build directory: /home/halmon/Pulpit/KK/OpenAL/build/external/OpenAL/utils/alsoft-config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
